ca37a68ed56c4fbc2124ab46fc9516d007146396
Dima <dimacheg06@gmail.com>
2022-10-29 00:11:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+var a = ['a', 'b', 'c'];
+var b = [1, 2, 3];
+var c = b.concat(a);
+console.log(c);
+console.log('__________________');
+
+
+console.log('next exercise');
+var arr = ['a', 'b', 'c'];
+arr.push(1, 2, 3);
+console.log(arr);
+console.log('__________________');
+
+
+console.log('next exercise');
+var arr = [1, 2, 3];
+arr.push(4, 5, 6);
+console.log(arr);
+console.log('__________________');
+
+
+console.log('next exercise');
+var arr = [1, 2, 3];
+arr.unshift(4, 5, 6);
+console.log(arr);
+console.log('__________________');
+
+
+console.log('next exercise');
+let mas = ['js', 'css', 'jq'];
+let elem = mas.shift();
+console.log(elem);
+console.log('__________________');
+
+
+console.log('next exercise');
+let m = ['js', 'css', 'jq'];
+let del = m.pop();
+console.log(del);
+console.log('__________________');
+
+
+console.log('next exercise');
+var obj = {js:'test', jq:'hello', css:'world'};
+console.log(Object.keys(obj));
+console.log('__________________');
+
+
+console.log('next exercise');
+var arr = [1, 2, 3, 4, 5];
+arr.splice(3, 4, 'a', 'b', 'c', 4, 5);
+console.log(arr);
+console.log('__________________');
+
+
+console.log('next exercise');
+var arr = [1, 2, 3, 4, 5];
+arr.splice(1, 4, 'a', 'b', 2, 3, 4, 'c', 5, 'e');
+console.log(arr);
